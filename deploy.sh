4c50ca82075f85c40c0c45d4dec8f7c1a349de16
xn213 <812725903@qq.com>
2019-08-26 02:21:04 +0800

    modified deploy.sh/ git push set SSH

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,8 @@ git add -A
 git commit -m ':smiley: deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
+# 如下 push 需配置 github SSH 
+# 生成 id_rsa.pub  `ssh-keygen -t rsa -b 4096 -C "your_email@example.com"`
 git push -f git@github.com:xn213/vue-test-pro.git master:gh-pages
 
 cd -
